1f990560de29635904e096d27c37b1aef7920ce5
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-10-28 22:51:24 +0200

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
 export { reportWebVitals } from 'next-axiom';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
