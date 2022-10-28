cbacba77f12c12fb7be3fde4bb7549d8154a6688
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-10-28 22:50:11 +0200

    Change footer name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer: React.FC = () => {
   return (
     <div className="flex justify-center border-t border-slate-100 py-8 mt-4 font-semibold">
       <span className="text-gray-500 hover:text-slate-800 transition-colors">
-        Created by
+        Made by
         <a
           className="pl-1 hover:underline"
           target="_blank"
