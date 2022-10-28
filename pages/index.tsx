b598dec8f33193bee68385a0a2f5c9474e30dc4d
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-10-28 22:39:07 +0200

    Add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,7 +66,31 @@ const Home: NextPage<{
       <Head>
         <title>When is the next F1 race?</title>
         <meta name="description" content="When is the next F1 race?" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <main className="dark:text-gray-200 text-neutral-900">
         <section className="relative bg-gray-200 dark:bg-neutral-900">
