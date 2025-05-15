12d31d88b2ee224116e90da6a1a519a802d909c2
Martijn Dorsman <martijn.dorsman@gmail.com>
2025-05-15 23:34:23 +0200

    Remove next-axiom integration from configuration and dependencies, simplifying the setup.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
-export { reportWebVitals } from 'next-axiom';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
